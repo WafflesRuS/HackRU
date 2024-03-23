5f2153eab3ca3407b7235a33168dc934828e76ca
AlandisAyupov <steamuservni@gmail.com>
2024-03-23 14:23:02 -0400

    First commit

--- recipe.py
+++ recipe.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+recipes = [
+    {
+
+    }
+]
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
