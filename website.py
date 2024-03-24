a159385307919bb39e5e4889b15da7a18a28dcc0
AlandisAyupov <steamuservni@gmail.com>
2024-03-23 20:14:02 -0400

    Website made prettier

--- website.py
+++ website.py
@@ -3,22 +3,11 @@ import requests
 
 app = Flask(__name__)
 
-url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients"
-
-querystring = {"ingredients":"apples,flour,sugar","number":"5","ignorePantry":"true","ranking":"2"}
-
-headers = {
-    "X-RapidAPI-Key": "2664f58407msha99b2252514da90p12ac7bjsnfcace937a84b",
-    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-recipes = response.json()
 
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template('home.html', recipes=recipes)
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def recipe_page():
+    return render_template('recipes.html')
